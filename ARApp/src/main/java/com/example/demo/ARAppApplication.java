5cd743df8578c9522143275ab53c236b6159b76a
tk07z13rf <48499875+tk07z13rf@users.noreply.github.com>
2021-06-05 17:46:00 +0900

    Add files via upload

---
+++ ARApp/src/main/java/com/example/demo/ARAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ARAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ARAppApplication.class, args);
+	}
+
+}
