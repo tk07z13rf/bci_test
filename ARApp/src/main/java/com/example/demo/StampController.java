96123e7ba05db96680a7e10f27744a8907400be7
tk07z13rf <48499875+tk07z13rf@users.noreply.github.com>
2021-06-06 20:04:46 +0900

    Add files via upload

---
+++ ARApp/src/main/java/com/example/demo/StampController.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class StampController {
+
+	@RequestMapping(value = "/stamp", method = RequestMethod.GET)
+	public ModelAndView stamp(ModelAndView mav) {
+		mav.setViewName("stamp");
+		return mav;
+
+	}
+
+	@RequestMapping(value = "/stamp/arcamera", method = RequestMethod.GET)
+	public ModelAndView arcamera(ModelAndView mav) {
+		mav.addObject("user", "user");
+		mav.setViewName("arcamera");
+		return mav;
+
+	}
+
+	@RequestMapping(value = "/stamp/stampget/*/{num}", method = RequestMethod.GET)
+	public ModelAndView stampget(@PathVariable int num, ModelAndView mav) {
+		mav.addObject("num", num);
+		mav.setViewName("stampget");
+		return mav;
+
+	}
+
+}
